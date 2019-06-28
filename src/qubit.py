ee6a6bf9f5a0a5cd5479dc8adb7a21f9c20f42ac
Aleksandar Kostovic <alexandar.kostovic@gmail.com>
2019-06-28 13:17:16 +0200

    Update qubit.py

--- src/qubit.py
+++ src/qubit.py
@@ -3,7 +3,7 @@
 import nazca as nd
 import nazca.demofab as demo
 
-with nd.Cell("Qubit") as qubit:
+with nd.Cell("MZI") as mzi:
     # Upper arm
     demo.shallow.sinebend(distance=100, offset=-40).put(0, 50)
     demo.shallow.strt(length=20).put()
@@ -33,29 +33,10 @@ with nd.Cell("Qubit") as qubit:
     nd.Pin('c2', pin=demo.eopm_dc().pin['c0']).put()
     nd.Pin('c3', pin=demo.eopm_dc().pin['c1']).put()
 
-with nd.Cell("Qubit2") as qubit2:
-    demo.shallow.sinebend(distance=100, offset=-40).put(0, 50)
-    demo.shallow.strt(length=20).put()
-    demo.shallow.sinebend(distance=100, offset=40).put()
-    demo.s2d().put()
-    ps = demo.eopm_dc(length=150).put()
-    demo.d2s().put()
-    demo.shallow.sinebend(distance=100, offset=200).put()
-    demo.shallow.strt(length=20).put()
-    demo.shallow.sinebend(distance=100, offset=-200).put()
-    # Pads
-    pad1 = demo.pad_dc().put(ps.pin["c0"].move(300, 200, 0))
-    demo.metaldc.sbend_p2p(ps.pin["c0"], Lstart=200).put()
-    pad2 = demo.pad_dc().put(ps.pin["c1"].move(300, 100, 0))
-    demo.metaldc.sbend_p2p(ps.pin["c1"], Lstart=200).put()
-    # Lower arm
-    demo.shallow.sinebend(distance=100, offset=40).put(0, -50)
-    demo.shallow.strt(length=20).put()
-    demo.shallow.sinebend(distance=100, offset=-40).put()
-    demo.shallow.strt(length=300).put()
-    demo.shallow.sinebend(distance=100, offset=-200).put()
-    demo.shallow.strt(length=20).put()
-    demo.shallow.sinebend(distance=100, offset=200).put()
+with nd.Cell("Qubit") as qubit:
+    mzi.put(0)
+    mzi.put(740)
+    mzi.put(1480)
 
     
 with nd.Cell("Direction Coupler") as dc:
@@ -67,23 +48,15 @@ with nd.Cell("Direction Coupler") as dc:
     demo.shallow.sinebend(distance=100, offset=200).put()
     
 qubit.put(0)
-qubit.put(740)
-qubit.put(1480)
 dc.put(2220)
 
 qubit.put(0, -520)
-qubit.put(740, -520)
-qubit.put(1480, -520)
 dc.put(2220, -520)
 
 qubit.put(0, -1040)
-qubit.put(740, -1040)
-qubit.put(1480, -1040)
 dc.put(2220, -1040)
 
 qubit.put(0, -1560)
-qubit.put(740, -1560)
-qubit.put(1480, -1560)
 dc.put(2220, -1560)
 
 nd.export_gds()
