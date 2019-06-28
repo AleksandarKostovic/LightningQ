fa0e4af023310fba95eaee792c9e405aa242888b
Aleksandar Kostovic <alexandar.kostovic@gmail.com>
2019-06-28 13:19:51 +0200

    Update qubit.py

--- src/qubit.py
+++ src/qubit.py
@@ -3,6 +3,7 @@
 import nazca as nd
 import nazca.demofab as demo
 
+# Mach-Zehnder Interferometer- MZI
 with nd.Cell("MZI") as mzi:
     # Upper arm
     demo.shallow.sinebend(distance=100, offset=-40).put(0, 50)
@@ -33,12 +34,13 @@ with nd.Cell("MZI") as mzi:
     nd.Pin('c2', pin=demo.eopm_dc().pin['c0']).put()
     nd.Pin('c3', pin=demo.eopm_dc().pin['c1']).put()
 
+# Qubit constructed from three MZIs
 with nd.Cell("Qubit") as qubit:
     mzi.put(0)
     mzi.put(740)
     mzi.put(1480)
 
-    
+# Directional Coupler
 with nd.Cell("Direction Coupler") as dc:
     demo.shallow.sinebend(distance=100, offset=200).put(0, 50)
     demo.shallow.strt(length=20).put()
